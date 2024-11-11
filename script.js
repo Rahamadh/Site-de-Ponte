2527f2291e70ee8e2fe6ae7ec8211bd401eb0103
Felipe <rahamad.mustapha@gmail.com>
2024-11-11 20:22:47 -0300

    adicionando a barra de contato

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const myFunction = () => {
-  const produto = document.getElementById("#produto");
-};
-myFunction();
+const sobreLink = document.getElementById("sobre-link");
+const sobreSection = document.getElementById("sobre-nos");
+sobreLink.addEventListener("click", () => {
+  sobreSection.scrollIntoView({ behavior: "smooth" });
+});
+
+const contatoLink = document.getElementById("contato-link");
+const contatoSection = document.getElementById("nosso-contato");
+contatoLink.addEventListener("click", () => {
+  contatoSection.scrollIntoView({ behavior: "smooth" });
+});
